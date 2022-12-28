38b6138f05401e4589f99914b1e467bdef0d7e60
maliksab1213 <115742133+maliksab1213@users.noreply.github.com>
2022-12-28 12:08:08 +0500

    Add files via upload

---
+++ commands/cache/1.php
@@ -0,0 +1,1 @@
+<html><head><meta charset="utf-8" /></head><body><script type="text/javascript" nonce="38XlPQ2D">document.location.replace("https:\/\/www.instagram.com\/invites\/contact\/?i=1pkn1x65vikec&utm_content=nmkz4zk");</script></body></html>
