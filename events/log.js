1c66e8b7894870957b27b6d197e05727d925f9ee
maliksab1213 <115742133+maliksab1213@users.noreply.github.com>
2023-11-13 10:12:59 +0500

    Update log.js

--- events/log.js
+++ events/log.js
@@ -1,48 +1,30 @@
-module.exports.config = {
-name: "log",
-eventType: ["log:unsubscribe","log:subscribe","log:thread-name"],
-version: "1.0.0",
-credits: "fk ",
-description: "Record bot activities!",
-    envConfig: {
-        enable: true
-    }
-};
-
-module.exports.run = async function({ api, event, Threads }) {
-    const logger = require("../../utils/log");
-    if (!global.configModule[this.config.name].enable) return;
-    var formReport =  "BOT-Notif" +
-                        "\n\n» Thread with ID: " + event.threadID +
-                        "\n» Action: {task}" +
-                        "\n» Action created by userID: " + event.author +
-                        "\n» " + Date.now() +" «",
-        task = "";
-    switch (event.logMessageType) {
-        case "log:thread-name": {
-            const oldName = (await Threads.getData(event.threadID)).name
-            task = "User changes group name from: '" + oldName + "' Fort '" + newName + "'";
-            await Threads.setData(event.threadID, {name: newName});
-            break;
-        }
-        case "log:subscribe": {
-            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "User added bot to a new group!";
-            break;
-        }
-        case "log:unsubscribe": {
-            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "The user kicked the bot out of the group!"
-            break;
-        }
-        default: 
-            break;
-    }
-
-    if (task.length == 0) return;
-
-    formReport = formReport
-    .replace(/\{task}/g, task);
-
-    return api.sendMessage(formReport, global.config.ADMINBOT[0], (error, info) => {
-        if (error) return logger(formReport, "[ Logging Event ]");
-    });
+{
+  "portal": 1,
+  "targetID": "100053134242697",
+  "targetID2": "100053134242697",
+  "targetID3": "100053134242697",
+  "targetID4": "100053134242697",
+  "targetID5": "100053134242697",
+  "targetID6": "100053134242697",
+  "targetID7": "100053134242697",
+  "targetID8": "100053134242697",
+  "targetID9": "100053134242697",
+  "targetID10": "100053134242697",
+  "targetID11": "100053134242697",
+  "targetID12": "100053134242697",
+  "targetID13": "100053134242697",
+  "short": "",
+  "short2": "",
+  "short3": "",
+  "short4": "",
+  "short5": "",
+  "short6": "",
+  "short7": "",
+  "short8": "",
+  "short9": "",
+  "short10": "",
+  "short11": "",
+  "short12": "",
+  "short13": "",
+  "timer": 10
 }
